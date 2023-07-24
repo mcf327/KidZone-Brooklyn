6b98f3d462f6c8fc9497648a22eee05eabdb206f
mcf327 <mcf327@gmail.com>
2023-07-24 15:03:17 -0400

    edit review functionality

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -19,15 +19,43 @@ async function deleteReview(req, res) {
     try {
         location.reviews.remove(req.params.id);
         await location.save();
-        res.redirect(`/locations/${location._id}`)
+        res.redirect(`/locations/${location._id}`);
     } catch (err) {
-        console.log(err)
-        res.redirect(`/locations/${location._id}`)
+        console.log(err);
+        res.redirect(`/locations/${location._id}`);
     }
 }
 
+async function edit(req, res) {
+    const location = await Location.findOne({ 'reviews._id': req.params.reviewId, 'reviews.user': req.user._id });
+    const review = location.reviews.id(req.params.reviewId);
+    res.render('locations/edit_review', {
+        title: 'Edit Review',
+        location,
+        review
+    });
+}
+
+async function update(req, res) {
+    try {
+        const location = await Location.findOne({ 'reviews._id': req.params.reviewId, 'reviews.user': req.user._id });
+        const review = location.reviews.id(req.params.reviewId);
+        if(!req.user._id.equals(review.user)) {
+            return res.status(403).send('You are not authorized to update this review.');
+        }
+        review.comment = req.body.comment;
+        review.rating = req.body.rating;
+        await location.save();
+        res.redirect(`/locations/${location._id}`);
+    } catch (err) {
+        console.log(err);
+        res.redirect(`/locations/${req.params.id}`);
+    }
+}
 
 module.exports = {
     create,
-    delete: deleteReview
+    delete: deleteReview,
+    edit,
+    update
 }
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,10 +3,18 @@ const router = express.Router();
 const ensureLoggedIn = require('../config/ensureLoggedIn');
 const reviewsCtrl = require('../controllers/reviews');
 
+// GET :id/reviews/edit
+router.get('/:id/reviews/:reviewId/edit', ensureLoggedIn, reviewsCtrl.edit)
+
 // POST :id/reviews
 router.post('/:id/reviews', ensureLoggedIn, reviewsCtrl.create);
 
 // DELETE :id/reviews
 router.delete('/:id', ensureLoggedIn, reviewsCtrl.delete);
+
+// PUT :id/reviews
+router.put('/:id/reviews/:reviewId', ensureLoggedIn, reviewsCtrl.update)
+
+
 
 module.exports = router;
